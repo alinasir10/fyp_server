bfeaafb2ad618bca0902c4d9b2b22c0758fc895b
Ali Nasir <60759115+alinasir10@users.noreply.github.com>
2024-10-29 16:52:54 +0500

    fixed redis service url credentials

--- src/lib/redis/index.ts
+++ src/lib/redis/index.ts
@@ -1,11 +1,4 @@
 import {Redis} from "ioredis";
 
-const serviceUri = "rediss://default:AVNS_e_Dpr_aJYN3LZTQP1fn@caching-d6b09f6-mr-3220.c.aivencloud.com:26417"
-
-export const redis = new Redis(serviceUri)
-// export const redis = new Redis({
-//     host: 'caching-d6b09f6-mr-3220.c.aivencloud.com',
-//     port: 26417,
-//     password: 'AVNS_e_Dpr_aJYN3LZTQP1fn',
-//     username: 'default'
-// })
+const serviceURL = `${process.env.REDIS_SERVICE_URL}` || 'redis://localhost:6379'
+export const redis = new Redis(serviceURL);
